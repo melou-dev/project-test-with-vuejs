544b53e48a71d8e06669b3e6519ed2227b95200d
melou-dev <ameliecuaces.dev@gmail.com>
2019-10-08 11:30:58 +0200

    create blockquote balise and linked to home.vue

--- src/components/HomeQuoteAboutLille.vue
+++ src/components/HomeQuoteAboutLille.vue
@@ -1,5 +1,7 @@
 <template>
-  
+  <blockquote cite="http://laureleroy.over-blog.com/2017/09/tu-sais-que-tu-viens-de-lille-quand.html">
+  <p> "Tu sais que tu es lilloise quand<br> tu commandes un pot'je vlees sans bafouiller"</p>
+  </blockquote>
 </template>
 
 <script>
@@ -9,5 +11,10 @@ export default {
 </script>
 
 <style>
+
+blockquote {
+    color: darkcyan;
+    font-size: 2rem;
+}
 
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
+    <HomeQuoteAboutLille />
     <HomeImage />
   </div>
 </template>
 
 <script>
 import HomeImage from '@/components/HomeImage.vue';
+import HomeQuoteAboutLille from '@/components/HomeQuoteAboutLille.vue';
 
 export default {
   name: 'home',
   components: {
    HomeImage,
+   HomeQuoteAboutLille,
   }
 }
 </script>
