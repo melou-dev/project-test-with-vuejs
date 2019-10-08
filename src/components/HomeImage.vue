ea34b10598a878ea0c88825e407907201a02d531
melou-dev <ameliecuaces.dev@gmail.com>
2019-10-08 10:57:58 +0200

    Project initial, create Contact.vue, linked all pages, create 2 components

---
+++ src/components/HomeImage.vue
@@ -0,0 +1,15 @@
+<template>
+  <figure>
+    <img src="../assets/images/photocvLilleGrandPlace.jpg" alt="Lille Grand Place">
+  </figure>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <HomeImage />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HomeImage from '@/components/HomeImage.vue';
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+   HomeImage,
   }
 }
 </script>
